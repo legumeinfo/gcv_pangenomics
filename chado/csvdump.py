867189aa81947023f91786d9212f35e7f9db8881
Alan Cleary <alan.cleary@msu.montana.edu>
2017-06-01 17:07:23 -0600

    Added Python script to dump GCV data from Chado

---
+++ chado/csvdump.py
@@ -0,0 +1,134 @@
+# Python
+import sys
+# SQLAlchemy
+from collections    import defaultdict
+from sqlalchemy     import create_engine
+from sqlalchemy.orm import sessionmaker
+# local
+from orm import Feature, FeatureLoc, GeneFamilyAssignment, GeneOrder, Organism
+
+
+def connect(user, password, db, host='localhost', port=5432):
+  '''Returns a session object'''
+  # connect view the PostgreSQL URL
+  url = 'postgresql://{}:{}@{}:{}/{}'
+  url = url.format(user, password, host, port, db)
+  # create a connection object
+  con = create_engine(url, client_encoding='utf8')
+  # create a session for the ORM
+  Session = sessionmaker(bind=con)
+  return Session()
+
+
+def dump(session):
+  # get all gene_orders
+  gene_orders = session.query(GeneOrder).all()
+  # create the initial gene objects
+  genes = dict((o.gene_id, {
+    'id':         o.gene_id,
+    'number':     o.number,
+    'chromosome': o.chromosome_id
+  }) for o in gene_orders)
+  gene_ids = list(genes.keys())
+
+  # get each gene's name via the feature table
+  gene_features = session.query(Feature.feature_id, Feature.name)\
+    .filter_by(Feature.feature_id.in_(gene_ids)).all()
+  for f in gene_features:
+    genes[f.feature_id]['name'] = f.name
+
+  # get each gene's fmin, fmax, and strand via the featureloc table
+  featurelocs = session.query(Featureloc)\
+    .filter_by(Featureloc.feature_id.in_(gene_ids)).all()
+  chromosome_ids = set()
+  for l in featurelocs:
+    g = genes[l.feature_id]
+    g['fmin']   = l.fmin
+    g['fmax']   = l.fmax
+    g['strand'] = l.strand
+    chromosome_ids.add(l.srcfeature_id)
+
+  # get each gene's family via the gene_family_assignment table
+  family_assignmentss = session.query(GeneFamilyAssignment)\
+    .filter_by(GeneFamilyAssignment.gene_id.in_(gene_ids)).all()
+  family_ids = set()
+  for f in family_assignments:
+    genes[f.gene_id]['family'] = f.family_label
+    family_ids.add(f.family_label)
+  families = [{'id': f, 'name': f} for f in family_ids]
+  dumpGenes(list(genes.values()))
+  dumpGeneFamilies(families)
+
+  # get all the chromosomes
+  chromosome_features = session.query(
+    Feature.feature_id,
+    Feature.name,
+    Feature.seqlen,
+    Feature.organism_id
+  ).filter_by(Feature.feature_id.in_(chromosome_ids)).all()
+  chromosomes = []
+  organism_ids = set()
+  for f in chromosome_features:
+    chromosomes.append({
+      'id':       f.feature_id,
+      'name':     f.name,
+      'length':   f.seqlen,
+      'organism': f.organism_id
+    })
+    organism_ids.add(f.organism_id)
+  dumpChromosomes(chromosomes)
+
+  # get all the organisms
+  raw_organisms = session.query(Organism)\
+    .filter_by(Organism.organism_id.in_(organism_ids)).all()
+  organisms = [{
+    'id':      o.organism_id,
+    'genus':   o.genus,
+    'species': o.species
+  } for o in raw_organisms]
+  dumpOrganisms(organisms)
+
+
+def dumpChromosomes(chromosomes):
+  headers = ['id', 'name', 'length', 'organism']
+  writeCSV('chromosomes', headers, chromosomes)
+
+
+def dumpGenes(genes):
+  headers = ['id', 'name', 'fmin', 'fmax', 'strand', 'number', 'family',
+    'chromosome']
+  writeCSV('genes', headers, genes)
+
+
+def dumpGeneFamilies(families):
+  headers = ['id', 'name']
+  writeCSV('families', headers, families)
+
+
+def dumpOrganisms(organisms):
+  headers = ['id', 'genus', 'species']
+  writeCSV('organisms', headers, organisms)
+
+
+def writeCSV(name, headers, data):
+  with open(name + '.csv', 'w') as f:
+    f.write(','.join(headers) + '\n')
+    for d in data:
+      line = [str(d[c]) for c in headers]
+      f.write(','.join(line) + '\n')
+
+
+if __name__ == '__main__':
+  argc = len(sys.argv)
+  if argc < 3:
+    sys.exit('{0} <user> <db> [host] [port]'.format(sys.argv[0]))
+  user, db = sys.argv[1:3]
+  host = 'localhost' if argc < 4 else sys.argv[3]
+  port = 5432
+  if argc >= 5:
+    try:
+      port = int(sys.argv[4])
+    except:
+      pass
+  session = connect(user, '', db, host=host, port=port)
+  dump(session)
---
+++ chado/orm.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, create_engine, ForeignKey, Integer, SmallInteger, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Feature(Base):
+  __tablename__ = 'feature'
+  feature_id = Column(Integer, primary_key=True)
+  organism_id = Column(Integer, ForeignKey('organism.organism_id'))
+  name = Column(String)
+  uniquename = Column(String)
+  seqlen = Column(Integer)
+ 
+ 
+class FeatureLoc(Base):
+  __tablename__ = 'featureloc'
+  featureloc_id = Column(Integer, primary_key=True)
+  feature_id = Column(Integer, ForeignKey('feature'))
+  srcfeature_id = Column(Integer, ForeignKey('feature'))
+  fmin = Column(Integer)
+  fmax = Column(Integer)
+  strand = Column(SmallInteger)
+
+
+class GeneFamilyAssignment(Base):
+  __tablename__ = 'gene_family_assignment'
+  gene_family_assignment_id = Column(Integer, primary_key=True)
+  gene_id = Column(Integer, ForeignKey('feature'))
+  family_label = Column(String)
+
+
+class GeneOrder(Base):
+  __tablename__ = 'gene_order'
+  gene_order_id = Column(Integer, primary_key=True)
+  chromosome_id = Column(Integer, ForeignKey('feature'))
+  gene_id = Column(Integer, ForeignKey('feature'))
+  number = Column(Integer)
+
+
+class Organism(Base):
+  __tablename__ = 'organism'
+  organism_id = Column(Integer, primary_key=True)
+  genus = Column(String)
+  species = Column(String)
