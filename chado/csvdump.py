a051e0eab04f3f5d86fffda02a6178182b38fb0c
Alan Cleary <alan.cleary@cs.montana.edu>
2017-06-02 10:45:50 -0500

    Fixed a few typos and added a "null_fam" gene family for orphan genes

--- chado/csvdump.py
+++ chado/csvdump.py
@@ -5,7 +5,7 @@ from collections    import defaultdict
 from sqlalchemy     import create_engine
 from sqlalchemy.orm import sessionmaker
 # local
-from orm import Feature, FeatureLoc, GeneFamilyAssignment, GeneOrder, Organism
+from orm import Feature, Featureloc, GeneFamilyAssignment, GeneOrder, Organism
 
 
 def connect(user, password, db, host='localhost', port=5432):
@@ -22,24 +22,26 @@ def connect(user, password, db, host='localhost', port=5432):
 
 def dump(session):
   # get all gene_orders
+  null_fam = -1
   gene_orders = session.query(GeneOrder).all()
   # create the initial gene objects
   genes = dict((o.gene_id, {
     'id':         o.gene_id,
     'number':     o.number,
-    'chromosome': o.chromosome_id
+    'chromosome': o.chromosome_id,
+    'family':     null_fam
   }) for o in gene_orders)
   gene_ids = list(genes.keys())
 
   # get each gene's name via the feature table
   gene_features = session.query(Feature.feature_id, Feature.name)\
-    .filter_by(Feature.feature_id.in_(gene_ids)).all()
+    .filter(Feature.feature_id.in_(gene_ids)).all()
   for f in gene_features:
     genes[f.feature_id]['name'] = f.name
 
   # get each gene's fmin, fmax, and strand via the featureloc table
   featurelocs = session.query(Featureloc)\
-    .filter_by(Featureloc.feature_id.in_(gene_ids)).all()
+    .filter(Featureloc.feature_id.in_(gene_ids)).all()
   chromosome_ids = set()
   for l in featurelocs:
     g = genes[l.feature_id]
@@ -49,9 +51,9 @@ def dump(session):
     chromosome_ids.add(l.srcfeature_id)
 
   # get each gene's family via the gene_family_assignment table
-  family_assignmentss = session.query(GeneFamilyAssignment)\
-    .filter_by(GeneFamilyAssignment.gene_id.in_(gene_ids)).all()
-  family_ids = set()
+  family_assignments = session.query(GeneFamilyAssignment)\
+    .filter(GeneFamilyAssignment.gene_id.in_(gene_ids)).all()
+  family_ids = set([null_fam])
   for f in family_assignments:
     genes[f.gene_id]['family'] = f.family_label
     family_ids.add(f.family_label)
@@ -65,7 +67,7 @@ def dump(session):
     Feature.name,
     Feature.seqlen,
     Feature.organism_id
-  ).filter_by(Feature.feature_id.in_(chromosome_ids)).all()
+  ).filter(Feature.feature_id.in_(chromosome_ids)).all()
   chromosomes = []
   organism_ids = set()
   for f in chromosome_features:
@@ -80,7 +82,7 @@ def dump(session):
 
   # get all the organisms
   raw_organisms = session.query(Organism)\
-    .filter_by(Organism.organism_id.in_(organism_ids)).all()
+    .filter(Organism.organism_id.in_(organism_ids)).all()
   organisms = [{
     'id':      o.organism_id,
     'genus':   o.genus,
